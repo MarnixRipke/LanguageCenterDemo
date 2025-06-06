bf166979dd59bed6ec113f122e7bb7f8c5e02b28
Marnix <191800038+MarnixRipke@users.noreply.github.com>
2025-06-06 12:00:56 +0200

    Starter project from Spring initializer

---
+++ src/main/java/nl/miwnn/ch16/marnix/languagecenterdemo/LanguageCenterDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch16.marnix.languagecenterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageCenterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageCenterDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/ch16/marnix/languagecenterdemo/LanguageCenterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch16.marnix.languagecenterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageCenterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
